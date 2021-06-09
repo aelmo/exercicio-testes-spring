8fc1426decb571932eff5fbe47858a546e5259e8
Amedeo Elmo <amedeo.elmo@mercadolivre.com>
2021-06-09 17:08:11 -0300

    Fixes Structure

--- aula-spring-manha-28052021/src/main/java/br/com/digitalhouse/FirstProject/repositories/PriceRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.digitalhouse.FirstProject.repositories;
-
-import br.com.digitalhouse.FirstProject.dtos.PriceDTO;
-import org.springframework.stereotype.Repository;
-
-
-public interface PriceRepository {
-    PriceDTO findPriceByLocation(String location);
-}
